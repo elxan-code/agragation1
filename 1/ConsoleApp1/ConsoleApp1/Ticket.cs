398f6b2b023ebb33ff9a67d20921e132a2048499
unknown <you@example.com>
2021-11-09 23:22:07 +0400

    mpc

---
+++ 1/ConsoleApp1/ConsoleApp1/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class Ticket
+    {
+        public Movie movie { get; set; }
+        public DataTime Date { get; set; }
+        public TimeSpan time { get; set; }
+        public double price { get; set; }
+        public DataTime selldate { get; set; }
+       
+
+       
+
+
+
+
+
+
+
+
+    }
+}
